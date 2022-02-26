d516f40055263fb9276d57cd2959619886c75b02
alesscom <90730443+alesscom@users.noreply.github.com>
2022-02-25 18:18:42 -0600

    Add files via upload

---
+++ kwatter.js
@@ -0,0 +1,6 @@
+
+function add_user(){
+    username= document.getElementById("user_name").value;
+    localStorage.setItem("user_name", user_name);
+    window.location="kwatter_room.html";
+}
---
+++ kwatter_room.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB6x8nhrLSmNQdAicCPqPXyLtnVLDZZgvM",
+    authDomain: "kwitter-f28cb.firebaseapp.com",
+    databaseURL: "https://kwitter-f28cb-default-rtdb.firebaseio.com",
+    projectId: "kwitter-f28cb",
+    storageBucket: "kwitter-f28cb.appspot.com",
+    messagingSenderId: "1059426995880",
+    appId: "1:1059426995880:web:61b0f4480fe6ac6cdb7c9d"
+  };
+  firebase.initializeApp(firebaseConfig);
+  room_name =localStorage.getItem("room_name");
+  user_name =localStorage.getItem("user_name");
+function enviar_room_name(){
+  
+}
